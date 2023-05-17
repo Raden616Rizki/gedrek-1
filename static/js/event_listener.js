b40325836b310200e0ba8d0be38ee952c94853eb
Raden616Rizki <rizkiraden616@gmail.com>
2023-05-17 13:54:20 +0700

    3# Commit Update Gravity

--- static/js/event_listener.js
+++ static/js/event_listener.js
@@ -4,39 +4,6 @@ const enterDoor = new Audio("../../static/music/enter-door.mp3")
 window.addEventListener('keydown', (event) => {
     // console.log(event.key);
     move(event.key)
-    // if (!player.preventInput) {
-    //     const key = event.key;
-
-    //     // jump
-    //     if (key === 'w' || key === 'ArrowUp') {
-
-    //         jump.play();
-
-    //         if (player.velocity.y === 0) {
-    //             player.velocity.y = -25;
-    //         }
-    //     } else if (key === 'a' || key === 'ArrowLeft') {
-    //         keys.a.pressed = true;
-    //     } else if (key === 'd' || key === 'ArrowRight') {
-    //         keys.d.pressed = true;
-    //     } else {
-    //         for (let i = 0; i < doors.length; i++) {
-    //             const door = doors[i];
-
-    //             if (player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width && player.hitbox.position.x >= door.position.x && player.hitbox.position.y + player.hitbox.height >= door.position.y && player.hitbox.position.y <= door.position.y + door.height) {
-    //                 // console.log('arrive');
-    //                 enterDoor.play();
-
-    //                 player.velocity.x = 0;
-    //                 player.velocity.y = 0;
-    //                 player.preventInput = true;
-    //                 player.switchSprite('enterDoor');
-    //                 door.play();
-    //                 return
-    //             }
-    //         }
-    //     }
-    // }
 });
 
 window.addEventListener('keyup', (event) => {
@@ -53,9 +20,12 @@ function move(way) {
 
             jump.play();
 
-            if (player.velocity.y === 0) {
-                player.velocity.y = -25;
-            }
+            // if (player.velocity.y === 0) {
+            //     player.velocity.y = -25;
+            // }
+
+            player.velocity.y = -25;
+
         } else if (key === 'a' || key === 'ArrowLeft') {
             keys.a.pressed = true;
         } else if (key === 'd' || key === 'ArrowRight') {
