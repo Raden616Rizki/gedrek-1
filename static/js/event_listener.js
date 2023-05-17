68fc81ecaa4b8a364ee207ce4918a72fac5eb8d2
Raden616Rizki <rizkiraden616@gmail.com>
2023-05-17 12:47:54 +0700

    2#  Commit Tambah fitur Touchscreen Movement

--- static/js/event_listener.js
+++ static/js/event_listener.js
@@ -3,12 +3,54 @@ const enterDoor = new Audio("../../static/music/enter-door.mp3")
 
 window.addEventListener('keydown', (event) => {
     // console.log(event.key);
+    move(event.key)
+    // if (!player.preventInput) {
+    //     const key = event.key;
+
+    //     // jump
+    //     if (key === 'w' || key === 'ArrowUp') {
+
+    //         jump.play();
+
+    //         if (player.velocity.y === 0) {
+    //             player.velocity.y = -25;
+    //         }
+    //     } else if (key === 'a' || key === 'ArrowLeft') {
+    //         keys.a.pressed = true;
+    //     } else if (key === 'd' || key === 'ArrowRight') {
+    //         keys.d.pressed = true;
+    //     } else {
+    //         for (let i = 0; i < doors.length; i++) {
+    //             const door = doors[i];
+
+    //             if (player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width && player.hitbox.position.x >= door.position.x && player.hitbox.position.y + player.hitbox.height >= door.position.y && player.hitbox.position.y <= door.position.y + door.height) {
+    //                 // console.log('arrive');
+    //                 enterDoor.play();
+
+    //                 player.velocity.x = 0;
+    //                 player.velocity.y = 0;
+    //                 player.preventInput = true;
+    //                 player.switchSprite('enterDoor');
+    //                 door.play();
+    //                 return
+    //             }
+    //         }
+    //     }
+    // }
+});
+
+window.addEventListener('keyup', (event) => {
+    // console.log(event.key);
+    stop(event.key);
+});
+
+function move(way) {
     if (!player.preventInput) {
-        const key = event.key;
+        const key = way;
 
         // jump
         if (key === 'w' || key === 'ArrowUp') {
-            
+
             jump.play();
 
             if (player.velocity.y === 0) {
@@ -36,27 +78,69 @@ window.addEventListener('keydown', (event) => {
             }
         }
     }
-});
+}
 
-window.addEventListener('keyup', (event) => {
-    // console.log(event.key);
-    switch(event.key) {
-        // Move Left
-        case 'a':
-            keys.a.pressed = false;
-        break
-        // Move Right
-        case 'd':
-            keys.d.pressed = false;
-        break
-        
-        // Move Left
-        case 'ArrowLeft':
-            keys.a.pressed = false;
-        break
-        // Move Right
-        case 'ArrowRight':
-            keys.d.pressed = false;
-        break
+function stop(way) {
+    if (way === 'a' || way === 'ArrowLeft') {
+        keys.a.pressed = false;
+    } else if (way === 'd' || way === 'ArrowRight') {
+        keys.d.pressed = false;
     }
-});
+}
+
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(event) {
+    xDown = event.touches[0].clientX;
+    yDown = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = event.touches[0].clientX;
+    var yUp = event.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    var way = "none"
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            // console.log('ArrowLeft');
+            // left
+            way = 'ArrowLeft';
+        } else {
+            // console.log('ArrowRight');
+            // right
+            way = 'ArrowRight';
+        }
+    } else {
+        if (yDiff > 0) {
+            // console.log('ArrowUp');
+            // Jump
+            way = 'ArrowUp';
+        } else {
+            // console.log('ArrowDown');
+            // Enter
+            way = 'ArrowDown';
+        }
+    }
+
+    move(way);
+
+    document.addEventListener("touchend", function () {
+        // console.log("end");
+        stop(way);
+    });
+
+    xDown = null;
+    yDown = null;
+}
--- static/js/touch.js
+++ static/js/touch.js
@@ -1,72 +1,48 @@
-// JavaScript
-var touchArea = document.getElementById("touch-area");
-
-// Menangani event sentuhan
-touchArea.addEventListener("touchstart", handleTouchStart, false);
-touchArea.addEventListener("touchmove", handleTouchMove, false);
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
 
 var xDown = null;
 var yDown = null;
 
 function handleTouchStart(event) {
-    xDown = event.touches[0].clientX;
-    yDown = event.touches[0].clientY;
+  xDown = event.touches[0].clientX;
+  yDown = event.touches[0].clientY;
 }
 
 function handleTouchMove(event) {
-    if (!xDown || !yDown) {
-        return;
-    }
+  if (!xDown || !yDown) {
+    return;
+  }
 
-    var xUp = event.touches[0].clientX;
-    var yUp = event.touches[0].clientY;
+  var xUp = event.touches[0].clientX;
+  var yUp = event.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
 
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-        if (xDiff > 0) {
-            console.log("ArrowLeft");
-
-            // Mengubah sentuhan ke kiri menjadi event.key "ArrowLeft"
-            var simulatedKeyEvent = new KeyboardEvent("keydown", {
-                key: "ArrowLeft",
-                bubbles: true,
-                cancelable: true,
-            });
-
-            // Men-trigger event keydown
-            document.dispatchEvent(simulatedKeyEvent);
-        } else {
-            console.log("ArrowRight");
-
-            // Mengubah sentuhan ke kanan menjadi event.key "ArrowRight"
-            var simulatedKeyEvent = new KeyboardEvent("keydown", {
-                key: "ArrowRight",
-                bubbles: true,
-                cancelable: true,
-            });
-
-            // Men-trigger event keydown
-            document.dispatchEvent(simulatedKeyEvent);
-        }
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+        // console.log('ArrowLeft');
+      // Mengubah pergerakan ke kiri menjadi event.key "ArrowLeft"
+      document.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowLeft" }));
     } else {
-        if (yDiff > 0) {
-            console.log("ArrowUp");
-            
-            // Mengubah sentuhan ke atas menjadi event.key "ArrowUp"
-            var simulatedKeyEvent = new KeyboardEvent("keydown", {
-                key: "ArrowUp",
-                bubbles: true,
-                cancelable: true,
-            });
-
-            // Men-trigger event keydown
-            document.dispatchEvent(simulatedKeyEvent);
-        }
+        // console.log('ArrowRight');
+      // Mengubah pergerakan ke kanan menjadi event.key "ArrowRight"
+      document.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowRight" }));
     }
+  } else {
+    if (yDiff > 0) {
+        // console.log('ArrowUp');
+      // Mengubah pergerakan ke atas menjadi event.key "ArrowUp"
+      document.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowUp" }));
+    } else {
+        // console.log('ArrowDown');
+      // Mengubah pergerakan ke bawah menjadi event.key "ArrowDown"
+      document.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowDown" }));
+    }
+  }
 
-    // Mengatur xDown dan yDown menjadi null agar tidak mempengaruhi sentuhan berikutnya
-    xDown = null;
-    yDown = null;
+  // Mengatur xDown dan yDown menjadi null agar tidak mempengaruhi sentuhan berikutnya
+  xDown = null;
+  yDown = null;
 }
